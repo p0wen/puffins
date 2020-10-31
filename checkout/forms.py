01423a741b750bb2b16ebe1341595f359a55cb8f
p0wen <Philly.e@hotmail.de>
2020-10-31 08:19:38 +0000

    feat: setup order form

---
+++ checkout/forms.py
@@ -0,0 +1,38 @@
+from django import forms
+from .models import Order
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ('first_name',
+                  'last_name', 'email', 'phone_number',
+                  'street_address1', 'street_address2',
+                  'zipcode', 'town_or_city', 'country',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-genereated labels
+        and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders ={
+            'first_name': 'Frist Name',
+            'last_name': 'Last Name',
+            'email': 'E-Mail Address',
+            'phone_number': 'Phone Number',
+            'street_address1': 'Street',
+            'street_address2': 'Apartment, suite, etc.',
+            'zipcode': 'Postal Code',
+            'town_or_city': 'City',
+            'country': 'Country',
+        } 
+
+        self.fields['email'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if not self.fields[field].required:
+                placeholder = f'{placeholders[field]} (Optional)'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
