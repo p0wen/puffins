ef46921625efa2ccc053f7b41811b96a943a6aec
p0wen <Philly.e@hotmail.de>
2020-11-03 12:39:48 +0000

    fix: typo on webhook intents

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 class StripeWH_Handler:
-    """ Handle Stripe webhooks """
+    """Handle Stripe webhooks"""
 
     def __init__(self, request):
         self.request = request
@@ -17,15 +17,15 @@ class StripeWH_Handler:
 
     def handle_payment_intent_succeeded(self, event):
         """
-        Handle the payment_intent.succeded webhook from Stripe
+        Handle the payment_intent.succeeded webhook from Stripe
         """
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
 
-    def handle_payment_intent_failed(self, event):
+    def handle_payment_intent_payment_failed(self, event):
         """
-        Handle the payment_intent.failed webhook from Stripe
+        Handle the payment_intent.payment_failed webhook from Stripe
         """
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
