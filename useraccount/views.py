68f569936ff724a5666ab087b6c427fdb89d78c3
p0wen <Philly.e@hotmail.de>
2020-11-03 23:15:20 +0000

    feat: basic profile template and urls setup

--- useraccount/views.py
+++ useraccount/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def profile(request):
+    template = 'useraccount/profile.html'
+    context = {}
+
+    return render(request, template, context)
