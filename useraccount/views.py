4ad13de15f1d1b308984e9fbb82592f5682c26f6
p0wen <Philly.e@hotmail.de>
2020-11-04 20:23:01 +0000

    feat: order history for customers

--- useraccount/views.py
+++ useraccount/views.py
@@ -1,15 +1,47 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from .models import UserAccount
+from .forms import UserAccountForm
+
+from checkout.models import Order
 
 
 def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserAccount, user=request.user)
 
+    if request.method == 'POST':
+        form = UserAccountForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+
+    form = UserAccountForm(instance=profile)
+    orders = profile.orders.all()
+
     template = 'useraccount/profile.html'
     context = {
-        'profile': profile,
+        'form': form,
+        'orders': orders,
+        'on_profile_page': True
+    }
+
+    return render(request, template, context)
+
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}. '
+        'A confirmation email was sent on the order date.'
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
     }
 
     return render(request, template, context)
