7c708f1e9b1b93736e809016a1f0ecb70623752d
p0wen <Philly.e@hotmail.de>
2020-11-04 20:19:40 +0000

    feat:storing data if user is authenticated and save info activated

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 
 from .models import Order, OrderLineItem
 from products.models import ProductVariant
+from useraccount.models import UserAccount
 
 import json
 import time
@@ -41,6 +42,20 @@ class StripeWH_Handler:
         fullname = shipping_details.name.split(' - ')
         firstname = fullname[0]
         lastname = fullname[1]
+
+        # Update profile information if save_info was checked
+        profile = None
+        username = intent.metadata.username
+        if username != 'AnonymousUser':
+            profile = UserAccount.objects.get(user__username=username)
+            if save_info:
+                profile.default_phone_number = shipping_details.phone
+                profile.default_country = shipping_details.address.country
+                profile.default_zipcode = shipping_details.address.postal_code
+                profile.default_town_or_city = shipping_details.address.city
+                profile.default_street_address1 = shipping_details.address.line1
+                profile.default_street_address2 = shipping_details.address.line2
+                profile.save()
 
         order_exists = False
         attempt = 1
