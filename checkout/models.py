9490a4f6317dca2a585b06dbbbda0c7de68301db
p0wen <Philly.e@hotmail.de>
2020-11-04 20:16:39 +0000

    feat: add original_cart, stripe_pid and user to checkout

--- checkout/models.py
+++ checkout/models.py
@@ -20,7 +20,7 @@ STATUS_OPTIONS = [
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
-    account_number = models.ForeignKey(UserAccount, on_delete=models.SET_NULL,
+    user_profile = models.ForeignKey(UserAccount, on_delete=models.SET_NULL,
                                        null=True, blank=True, related_name='orders')
     order_status = models.CharField(max_length=1, choices=STATUS_OPTIONS, default='0')
     first_name = models.CharField(max_length=50, null=False, blank=False)
@@ -38,7 +38,9 @@ class Order(models.Model):
     delivery_cost = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     date_order_placed = models.DateTimeField(auto_now_add=True)
-
+    original_cart = models.TextField(null=False, blank=False, default='')
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
+    
     def _generate_order_number(self):
         """
         Generate a random, unique order number using UUID
