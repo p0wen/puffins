9f763d47c1507aa7c2a742ed2399c7dd3a825cea
p0wen <Philly.e@hotmail.de>
2020-11-04 20:15:00 +0000

    fix: add original cart and stripe_pid to admin view

--- checkout/admin.py
+++ checkout/admin.py
@@ -10,20 +10,26 @@ class OrderAdminInline(admin.TabularInline):
 class OrderAdmin(admin.ModelAdmin):
     inlines = (OrderAdminInline,)
 
-    readonly_fields = ('order_number', 'date_order_placed',
+    readonly_fields = ('order_number',  'date_order_placed',
                        'delivery_cost', 'total_order',
-                       'grand_total', 'total_tax', 'tax_rate')
+                       'grand_total', 'total_tax', 'tax_rate',
+                       'original_cart',
+                       'stripe_pid')
 
-    fields = ('order_number', 'order_status', 'date_order_placed',
+    fields = ('order_number', 'user_profile',
+              'order_status', 'date_order_placed',
               'first_name', 'last_name', 'email', 'phone_number',
               'street_address1', 'street_address2',
               'zipcode', 'town_or_city', 'country',
-              'delivery_cost', 'total_order', 'grand_total')
+              'delivery_cost', 'total_order', 'grand_total', 'original_cart',
+              'stripe_pid')
 
     list_display = ('order_number', 'order_status', 'date_order_placed',
-                    'first_name', 'last_name',
+                    'user_profile',
                     'total_order', 'delivery_cost',
                     'grand_total', 'total_tax', 'tax_rate')
+
+    ordering = ('-date_order_placed',)
 
 
 admin.site.register(Order, OrderAdmin)
