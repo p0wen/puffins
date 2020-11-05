8dc3a41abed4268c93a72f25a5921f85ac1c9a7c
p0wen <Philly.e@hotmail.de>
2020-11-05 01:48:33 +0000

    fix: remove wish list from project urls

--- puffins/urls.py
+++ puffins/urls.py
@@ -27,5 +27,4 @@ urlpatterns = [
     path('cart/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
     path('useraccount/', include('useraccount.urls')),
-    path('wishlist/', include('wishlist.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
