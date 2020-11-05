131f1837f0a4d1095518bbab44d13957e76ccac2
p0wen <Philly.e@hotmail.de>
2020-11-05 01:45:26 +0000

    fix: formatting of urlpattern

--- cart/urls.py
+++ cart/urls.py
@@ -6,7 +6,5 @@ urlpatterns = [
     path('add/', views.add_to_cart, name="add_to_cart"),
     path('increase_qty/<productvariant>/', views.increase_quantity_by_one, name='increase_quantity_by_one'),
     path('decrease_qty/<productvariant>/', views.decrease_quantity_by_one, name='decrease_quantity_by_one'),
-    path('remove/<productvariant>/',
-         views.remove_from_cart,
-         name='remove_from_cart'),
+    path('remove/<productvariant>/',views.remove_from_cart, name='remove_from_cart'),
 ]
