d86ce804f5db1473fa7aa4daf15dbc5d1b138d7a
p0wen <Philly.e@hotmail.de>
2020-11-06 22:40:53 +0000

    fix: rename urls for wishlist and add/remove wish

--- wishlists/urls.py
+++ wishlists/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.view_wishlist, name="view_wishlist"),
-    path('add_to_wishlist/<product_id>', views.add_to_wishlist, name="add_to_wishlist"),
+    path('', views.view_wishlist, name="wishlist"),
+    path('add_wish/<product_id>', views.add_wish, name="add_wish"),
 ]
