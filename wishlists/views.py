d69acdcdef2501108174481a6547be4e0e6422c6
p0wen <Philly.e@hotmail.de>
2020-11-06 23:45:14 +0000

    fix: remove unused import

--- wishlists/views.py
+++ wishlists/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .models import UserWishlist
 from django.http import HttpResponseRedirect
 from products.models import Product
 from useraccount.models import UserAccount
