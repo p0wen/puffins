d503bf241aab49b8fb21e7cd835387e7985e1a26
p0wen <Philly.e@hotmail.de>
2020-11-11 16:05:54 +0000

    feat: ajax response on succesful form submit

--- contact/views.py
+++ contact/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse
+from django.template.loader import render_to_string
+from django.core.mail import send_mail, BadHeaderError
 from django.conf import settings
 from .forms import ContactForm
 from useraccount.models import UserAccount
@@ -6,35 +8,55 @@ from .models import FAQ
 
 
 def contact(request):
-    template = 'contact/contact.html'
 
-    general = FAQ.objects.all().filter(category=1)
-    return_policy = FAQ.objects.all().filter(category=2)
-    pre_order = FAQ.objects.all().filter(category=3)
-    delivery = FAQ.objects.all().filter(category=4)
-    payment = FAQ.objects.all().filter(category=5)
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = form.cleaned_data['category']
+            from_email = form.cleaned_data['email']
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            message = form.cleaned_data['message']
+            try:
+                send_mail(
+                    subject,
+                    f"Message from: {first_name} {last_name}\n Message: \n {message}", 
+                    from_email,
+                    [settings.DEFAULT_FROM_EMAIL])
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
+            render_success = render_to_string('contact/contact_success.html')
+            return HttpResponse(render_success, content_type="text/html")
 
-    if request.user.is_authenticated:
-        profile = UserAccount.objects.get(user=request.user)
-        user_email = profile.user.email
-        contact_form = ContactForm(initial={
-            'first_name': profile.user.first_name,
-            'last_name': profile.user.last_name,
-            'email': user_email,
-            })
     else:
-        contact_form = ContactForm()
+        template = 'contact/contact.html'
+        general = FAQ.objects.all().filter(category=1)
+        return_policy = FAQ.objects.all().filter(category=2)
+        pre_order = FAQ.objects.all().filter(category=3)
+        delivery = FAQ.objects.all().filter(category=4)
+        payment = FAQ.objects.all().filter(category=5)
 
-    context = {
-        'contact_form': contact_form,
-        'preorder': pre_order,
-        'general': general,
-        'return': return_policy,
-        'delivery': delivery,
-        'payment': payment,
-    }
+        if request.user.is_authenticated:
+            profile = UserAccount.objects.get(user=request.user)
+            user_email = profile.user.email
+            contact_form = ContactForm(initial={
+                'first_name': profile.user.first_name,
+                'last_name': profile.user.last_name,
+                'email': user_email,
+                })
+        else:
+            contact_form = ContactForm()
 
-    return render(request, template, context)
+        context = {
+            'contact_form': contact_form,
+            'preorder': pre_order,
+            'general': general,
+            'return': return_policy,
+            'delivery': delivery,
+            'payment': payment,
+        }
+
+        return render(request, template, context)
 
 
 def contact_success(request):
