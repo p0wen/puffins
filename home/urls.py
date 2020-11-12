6fe84b198f64923781854d10fa6a8c52e752ef92
p0wen <Philly.e@hotmail.de>
2020-11-12 19:30:18 +0000

    fix: add discontinued filter and format text

--- home/urls.py
+++ home/urls.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 from django.contrib import admin
 from django.urls import path
 from . import views
--- home/views.py
+++ home/views.py
@@ -1,15 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render
 from products.models import Product
 import random
-
-# Create your views here.
 
 
 def index(request):
     """ A view to return the index page """
 
     req_no_of_random_items = 4
-    qs = Product.objects.filter(is_featured=True)
+    qs = Product.objects.filter(is_featured=True, discontinued=False)
     possible_ids = list(qs.values_list('id', flat=True))
     possible_ids = random.choices(possible_ids, k=req_no_of_random_items)
     random_featured_product = qs.filter(pk__in=possible_ids)
