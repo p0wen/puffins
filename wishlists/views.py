5f8a6808e321cd0fe273ab521a8a53b983bf79f0
p0wen <Philly.e@hotmail.de>
2020-11-12 20:05:50 +0000

    fix: code format and add comment

--- wishlists/views.py
+++ wishlists/views.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
@@ -13,15 +16,20 @@ def view_wishlist(request):
     wishlist = Product.objects.filter(userwishlists__user_profile=user)
     context = {
         'wishlist': wishlist,
-        }
+    }
     return render(request, 'wishlists/wishlist.html', context)
 
 
 @login_required
 def add_wish(request, product_id):
+    """
+    Allows users to add or remove a product to/from their personal wishlist.\n
+    """
+
     wish = get_object_or_404(Product, pk=product_id)
     user = UserAccount.objects.get(user=request.user)
-    wishlist_user, created = UserWishlist.objects.get_or_create(user_profile=user)
+    wishlist_user, created = UserWishlist.objects.get_or_create(
+        user_profile=user)
     wishlist = Product.objects.filter(userwishlists__user_profile=user)
 
     if created:
