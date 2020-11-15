b0f8f978f69ca005c9f43df9a789d53a23817386
p0wen <Philly.e@hotmail.de>
2020-11-15 10:48:20 +0000

    test: format test code

--- blog/test_views.py
+++ blog/test_views.py
@@ -18,10 +18,10 @@ class TestContactView(TestCase):
 
     def test_get_post(self):
         myuser = UserAccount.objects.get(user=1)
-        post = BlogPost.objects.create(author=myuser,
-                                       title="Test-Post",
-                                       subtitle="Does this work?",
-                                       body="This is a post",)
+        BlogPost.objects.create(author=myuser,
+                                title="Test-Post",
+                                subtitle="Does this work?",
+                                body="This is a post",)
         response = self.client.get('/blog/test-post')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'blog/show_post.html')
