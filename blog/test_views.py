f4ac8d26a064da2ac5000703f91777df5d42ed71
p0wen <Philly.e@hotmail.de>
2020-11-15 10:47:37 +0000

    test: add tests for blog views

--- blog/test_views.py
+++ blog/test_views.py
@@ -1,9 +1,27 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import BlogPost
+from useraccount.models import UserAccount
 
 
 class TestContactView(TestCase):
+
+    def setUp(self):
+        password = 'mypassword'
+        User.objects.create_superuser('myuser', 'myemail@test.com', password)
+        self.client.login(username='myuser', password=password)
 
     def test_get_blog_overview(self):
         response = self.client.get('/blog/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'blog/blog.html')
+
+    def test_get_post(self):
+        myuser = UserAccount.objects.get(user=1)
+        post = BlogPost.objects.create(author=myuser,
+                                       title="Test-Post",
+                                       subtitle="Does this work?",
+                                       body="This is a post",)
+        response = self.client.get('/blog/test-post')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'blog/show_post.html')
