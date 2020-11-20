f5089161390496543f6820e994f17b60f7230384
p0wen <Philly.e@hotmail.de>
2020-11-20 20:02:56 +0000

    fix: add inline comments and fix formatting issues

--- products/static/products/js/products.js
+++ products/static/products/js/products.js
@@ -1,4 +1,6 @@
-$( document ).ready(function() {
+// jQuery for Cart
+
+$(document).ready(function() {
     $('#add_to_cart').on('submit', function(event){
         event.preventDefault();
         addToCart();
@@ -6,20 +8,20 @@ $( document ).ready(function() {
     });
 });
 
- function addToCart() {
-        form = $('#add_to_cart').closest("form");
-        productvariant_value = $('#order_item').val()
-        $.ajax({
-            url : form.attr("data-sent-to-url"), // the endpoint
-            type : "POST", // http method
-            data : { productvariant : productvariant_value }, // data sent with the post request
-            // handle a successful response
-            success : function(html) {
-                $('#sideDrawerContent').html(html)
-            },
-
-            // handle a non-successful response
-            error : function(xhr,errmsg,err) {
-            }
-        });
-    };
+// Forward the form request with the productvariant
+function addToCart() {
+    form = $('#add_to_cart').closest("form");
+    productvariant_value = $('#order_item').val()
+    $.ajax({
+        url: form.attr("data-sent-to-url"), // the endpoint
+        type: "POST", // http method
+        data: { productvariant: productvariant_value }, // data sent with the post request
+        // handle a successful response
+        success: function (html) {
+            $('#sideDrawerContent').html(html)
+        },
+        // handle a non-successful response
+        error: function (xhr, errmsg, err) {
+        }
+    });
+};
